cfed7e77f252f6d7002a53ad38f997a267c17b36
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-03-11 14:13:39 -0700

    added ready body

--- test_movement.gd
+++ test_movement.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hello everyone!")
+	print("hello everyone!, how are you?")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
